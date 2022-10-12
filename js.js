2f364a96f6559cc33510b27a808935f82a8c944b
Anton0218 <101352098+Anton0218@users.noreply.github.com>
2022-10-12 14:49:07 +0600

    Add files via upload

--- js.js
+++ js.js
@@ -1,13 +1,15 @@
-let inputs = document.querySelectorAll('.RegForm__input');
-let btn = document.querySelector('.RegForm__btn');
+;( function () { 
+    let inputs = document.querySelectorAll('.RegForm__input');
+    let btn = document.querySelector('.RegForm__btn');
 
-btn.addEventListener('click', function (event) {
-    for (let input of inputs) {
-        if (input.value == '') {
-            for (let input of inputs) {
-                input.classList.add('error');
+    btn.addEventListener('click', function (event) {
+        for (let input of inputs) {
+            if (input.value == '') {
+                for (let input of inputs) {
+                    input.classList.add('error');
+                }
+                event.preventDefault();
             }
-            event.preventDefault();
         }
-    }
-});
+    });
+})();
